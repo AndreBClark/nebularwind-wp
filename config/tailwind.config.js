fc08c9ad4449acbdd97685164385e3715ee018a1
AndreBClark <andre@cosmicdivision.dev>
2020-02-02 22:49:39 -0800

    :wrench: add bigger spacing utilities

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -3,6 +3,13 @@ const darken = require('polished/lib/color/darken');
 module.exports = {
     theme: {
         extend: {
+            spacing: {
+                '72': '18rem',
+                '84': '21rem',
+                '96': '24rem',
+                '108': '27rem',
+                '120': '30rem'
+            },
             colors: {
                 deeppurple: {
                     900: darken(.25, "#44337a"),
