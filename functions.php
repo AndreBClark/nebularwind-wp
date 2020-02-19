c7390a0233cc0c40a212f6585682bed877ada33c
AndreBClark <andre@cosmicdivision.dev>
2020-02-18 22:23:05 -0800

    :sparkles: implement woocommerce support

--- functions.php
+++ functions.php
@@ -163,5 +163,16 @@ class StarterSite extends Timber\Site {
 	}
 
 }
+function theme_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
 
+add_action( 'after_setup_theme', 'theme_add_woocommerce_support' );
+function timber_set_product( $post ) {
+    global $product;
+
+    if ( is_woocommerce() ) {
+        $product = wc_get_product( $post->ID );
+    }
+}
 new StarterSite();
