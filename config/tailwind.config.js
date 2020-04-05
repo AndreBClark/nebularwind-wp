a5720125a746bd3029e4db29a6c99c0cacb45dca
AndreBClark <andre@cosmicdivision.dev>
2020-04-05 00:08:48 -0700

    Update tailwind.config.js

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -4,24 +4,30 @@ module.exports = {
     theme: {
         extend: {
             screens: {
-                '2xl': '1680px'
+
             },
             container: {
                 center: true,
             },
             fontFamily: {
-                headingone: "'Inter var'",
-                headingtwo: "'Inter var'"
+                body: "Inter var",
+                headingone: "Inter var",
+                headingtwo: "Inter var"
             },
             spacing: {
                 '72': '18rem',
                 '84': '21rem',
                 '96': '24rem',
                 '108': '27rem',
-                '120': '30rem'
+                '120': '30rem',
+                '132': '33rem',
+                '144': '36rem',
+                '156': '39rem',
+                '168': '42rem',
             },
             colors: {
                 'primary': "#bc2e7c",
+                'secondary': "#350e47"
             }
         }
     },
