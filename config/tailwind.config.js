f6dabf3b0df0d2585b6d5d5908b39cf00693d002
AndreBClark <andre@cosmicdivision.dev>
2020-04-08 22:32:26 -0700

    tailwind mixblendmode

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -7,9 +7,14 @@ module.exports = {
                 center: true,
             },
             fontFamily: {
-                body: "Inter var",
-                headingone: "Inter var",
-                headingtwo: "Inter var"
+                body: "Inter",
+                heading: "Inter",
+            },
+            fontSize: {
+                "7xl": '5rem',
+                "8xl": '6rem',
+                '9xl': '7rem',
+                '10xl': '8rem'
             },
             spacing: {
                 '72': '18rem',
@@ -21,13 +26,18 @@ module.exports = {
                 '144': '36rem',
                 '156': '39rem',
                 '168': '42rem',
+                '180': '45rem'
             },
             colors: {
                 'primary': "#bc2e7c",
-                'secondary': "#350e47"
+                'secondary': "#350e47",
+                'dark': "#1a202c"
             }
         }
     },
-    variants: {},
-    plugins: []
+    variants: {
+    },
+    plugins: [
+        require('tailwindcss-blend-mode')()
+    ]
 }
